fdca2a2a725419bda31889e27b26b22e830a4d4c
Кирилл Качанов <Kachanov0b@gmail.com>
2025-07-10 19:31:22 +0300

    Практическое задание 2. Реализовал страницу Hello, world

---
+++ tgBot/src/test/java/ru/KirillKachanov/tgBot/TgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.KirillKachanov.tgBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
