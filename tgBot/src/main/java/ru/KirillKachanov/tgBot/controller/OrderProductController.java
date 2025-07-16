e5131ea0bb2e69ee95dbc949981aefbb9fe91549
Кирилл Качанов <Kachanov0b@gmail.com>
2025-07-16 14:54:49 +0300

    вернул контроллер

---
+++ tgBot/src/main/java/ru/KirillKachanov/tgBot/controller/OrderProductController.java
@@ -0,0 +1,29 @@
+package ru.KirillKachanov.tgBot.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.KirillKachanov.tgBot.entity.OrderProduct;
+import ru.KirillKachanov.tgBot.repository.OrderProductRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/order-products")
+public class OrderProductController {
+
+    private final OrderProductRepository repository;
+
+    public OrderProductController(OrderProductRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderProduct> createOrderProduct(@RequestBody OrderProduct orderProduct) {
+        OrderProduct savedOrder = repository.save(orderProduct);
+        return new ResponseEntity<>(savedOrder, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<OrderProduct> getAllOrderProducts() {
+        return repository.findAll();
+    }
+}
